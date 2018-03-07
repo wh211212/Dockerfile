79acd4a0de2c656bc4022b31cd17ed4fb28f44f8
shaonbean <wh211212@qq.com>
2018-03-07 16:16:29 +0800

    add nginx dockerfile

---
+++ nginx/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# stop aniu/nginx container && delete container&images
+
+containerid=`docker ps -a | grep aniu/nginx | awk '{print $1}'`
+
+sudo docker stop $containerid
+
+sudo docker rm $containerid
+
+sudo docker rmi  aniu/nginx
